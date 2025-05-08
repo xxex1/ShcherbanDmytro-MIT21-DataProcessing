56fab4b4ceda77cd18796745c94389a83221410c
Dmytro <dimasahah15@gmail.com>
2025-05-08 19:24:22 +0300

    init commit

---
+++ Lab1/src/main/java/com/example/servletdemo/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.example.servletdemo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().write("<h1>Hello from HelloServlet!</h1>");
+    }
+}
